10f027cd5c96ba2fa0f361a4c9ddbfd49594f0ae
Ustariz Enzo <enzo.ustariz@hotmail.fr>
2020-12-31 10:32:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,79 +1,13 @@
-import { useState } from "react";
 import "./App.css";
+import Tabs from './Tabs';
 
 function App() {
-  const [toggleTabs, setToggleTabs] = useState(1);
-
-  const toggleTab = (index) => {
-    setToggleTabs(index);
-  };
 
   return (
     <div className="App">
-      <div className="container">
-        <div className="bloc-onglets">
-          <div
-            className={toggleTabs === 1 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(1)}
-          >
-            Onglet 1
-          </div>
-          <div
-            className={toggleTabs === 2 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(2)}
-          >
-            Onglet 2
-          </div>
-          <div
-            className={toggleTabs === 3 ? "tabs active-tabs" : "tabs"}
-            onClick={() => toggleTab(3)}
-          >
-            Onglet 3
-          </div>
-        </div>
-
-        <div className="contenu-onglets">
-          <div
-            className={toggleTabs === 1 ? "contenu active-contenu" : "contenu"}
-          >
-            <h2>Contenu 1</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-              praesentium incidunt quia aspernatur quasi quidem facilis quo
-              nihil vel voluptatum?
-            </p>
-          </div>
-
-          <div
-            className={toggleTabs === 2 ? "contenu active-contenu" : "contenu"}
-          >
-            <h2>Contenu 2</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-              voluptatum qui adipisci.
-            </p>
-          </div>
-
-          <div
-            className={toggleTabs === 3 ? "contenu active-contenu" : "contenu"}
-          >
-            <h2>Contenu 3</h2>
-            <hr />
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
-              nostrum rerum laudantium totam unde adipisci incidunt modi alias!
-              Accusamus in quia odit aspernatur provident et ad vel distinctio
-              recusandae totam quidem repudiandae omnis veritatis nostrum
-              laboriosam architecto optio rem, dignissimos voluptatum beatae
-              aperiam voluptatem atque. Beatae rerum dolores sunt.
-            </p>
-          </div>
-        </div>
-      </div>
+        <Tabs />
     </div>
   );
 }
-
+    
 export default App;
