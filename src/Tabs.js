c533b03ab0e4f8963f8d0e9972593190a008e1ef
Ustariz Enzo <enzo.ustariz@hotmail.fr>
2020-12-31 10:32:23 +0100

    Create Tabs.js

---
+++ src/Tabs.js
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import "./App.css";
+
+function Tabs() {
+
+  const [toggleState, setToggleState] = useState(1);
+
+    const toggleTab = (index) => {
+        setToggleState(index);
+    }
+
+  return (
+
+      <div className="container">
+          
+        <div className="bloc-tabs">
+          <div 
+          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(1)}
+          >Tab 1</div>
+          <div 
+         className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(2)}
+          >Tab 2</div>
+          <div 
+         className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab(3)}
+          >Tab 3</div>
+        </div>
+
+        <div className="content-tabs">
+
+          <div 
+          className={toggleState === 1 ? "content  active-content" : "content"}
+          >
+            <h2>Content 1</h2>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+              praesentium incidunt quia aspernatur quasi quidem facilis quo
+              nihil vel voluptatum?
+            </p>
+          </div>
+
+          <div 
+          className={toggleState === 2 ? "content  active-content" : "content"}
+          >
+            <h2>Content 2</h2>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+              voluptatum qui adipisci.
+            </p>
+          </div>
+
+          <div 
+          className={toggleState === 3 ? "content  active-content" : "content"}
+          >
+            <h2>Content 3</h2>
+            <hr />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sed
+              nostrum rerum laudantium totam unde adipisci incidunt modi alias!
+              Accusamus in quia odit aspernatur provident et ad vel distinctio
+              recusandae totam quidem repudiandae omnis veritatis nostrum
+              laboriosam architecto optio rem, dignissimos voluptatum beatae
+              aperiam voluptatem atque. Beatae rerum dolores sunt.
+            </p>
+          </div>
+          
+        </div>
+      </div>
+ 
+  );
+}
+
+export default Tabs;
